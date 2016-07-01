796b632d2011f764124bc7bf5de2794858030253
Ryan Clarke <ryclarke@cisco.com>
2016-07-01 14:06:16 -0400

    [chore] Add ability to test single endpoint
    Set the `TEST` environment variable to the desired API endpoint
    name to only run the unit tests for that endpoint. This is useful
    when debugging issues with a specific endpoint and running the
    full test suite to check every modification would be inefficient.
    
    Example: `TEST=pods npm test`

--- test/integrated.js
+++ test/integrated.js
@@ -24,33 +24,38 @@ try {
 describe('Cisco Kubernetes Client', function () {
     var client = Client(config);
 
-    // Test assorted client capabilities
-    describe('Miscellaneous', function () {
-        // Initialize the client and test namespace
-        before(function () {
-        });
+    // If `TEST` environment variable is set, only test the given endpoint
+    if (client.hasOwnProperty(process.env.TEST)) {
+        testEndpoint(process.env.TEST, client);
+    } else {
+        // Test assorted client capabilities
+        describe('Miscellaneous', function () {
+            // Initialize the client and test namespace
+            before(function () {
+            });
 
-        // Initialize the client as a Promise
-        it('should return a Promise of the client', function () {
-            return Client(Object.merge({usePromise: true}, config, true, false));
-        });
-        // Use a method with a Node.js callback
-        it('should use a Node.js callback', function (done) {
-            client.pods.get(function (error, resource) {
-                if (error) {
-                    done(error);
-                } else {
-                    should(resource).have.property('items');
-                    done();
-                }
+            // Initialize the client as a Promise
+            it('should return a Promise of the client', function () {
+                return Client(Object.merge({usePromise: true}, config, true, false));
+            });
+            // Use a method with a Node.js callback
+            it('should use a Node.js callback', function (done) {
+                client.pods.get(function (error, resource) {
+                    if (error) {
+                        done(error);
+                    } else {
+                        should(resource).have.property('items');
+                        done();
+                    }
+                });
             });
         });
-    });
 
-    // Test all client endpoints
-    for (var endpoint in client) {
-        if (client.hasOwnProperty(endpoint) && !endpoint.startsWith('proxy/')) {
-            testEndpoint(endpoint, client);
+        // Test all client endpoints
+        for (var endpoint in client) {
+            if (client.hasOwnProperty(endpoint) && !endpoint.startsWith('proxy/')) {
+                testEndpoint(endpoint, client);
+            }
         }
     }
 });
